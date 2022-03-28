9ff28a03691f60486d653a5c2beb2e2086cc51d7
alberttat <76408824+alberttat@users.noreply.github.com>
2022-03-28 14:40:11 -0700

    Current 65% keyboard layout functional
    does not handle capitalized letters

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 "use strict";
 console.log("hello");
 
-let keyEvent;
+let keyEvent, keyName, keyLocation;
 
 function keyboardInput(keyPressed) {
     keyEvent = document.getElementById(keyPressed);
-    console.log(keyEvent);
     keyEvent.style.color = "green";
+    keyName = "";
+    keyLocation = "";
 }
 
 
 
 document.addEventListener('keydown', (event) => {
-    const keyName = event.key;
-    console.log(keyName);
+    keyLocation = event.location;
+    if (keyLocation === 1) keyName = 'L';
+    if (keyLocation === 2) keyName = 'R';
+    if (keyLocation === 0) keyName = '';
+
+    keyName = keyName + event.key;
+    if (event.key === 'Meta') keyName = 'Meta';
     keyboardInput(keyName);
+
 });
+
