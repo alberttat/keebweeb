a22526953b36daa5e00d3195bd80636aa54a74ed
alberttat <76408824+alberttat@users.noreply.github.com>
2022-04-04 22:14:57 -0700

    Refactor 65%
    redid html & css to reflect grid refactor
    js now handles both lower & upper case letters

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 "use strict";
-console.log("hello");
 
-let keyEvent, keyName, keyLocation;
+
+let keyEvent, keyName, keyLocation, keyboardSize;
+const keyboard = document.getElementById("keyboardSize");
+
 
 function keyboardInput(keyPressed) {
     keyEvent = document.getElementById(keyPressed);
-    keyEvent.style.color = "green";
+    keyEvent.style.color = 'green';
+
+    console.log(keyEvent);
     keyName = "";
     keyLocation = "";
 }
 
-
-
 document.addEventListener('keydown', (event) => {
     keyLocation = event.location;
-    if (keyLocation === 1) keyName = 'L';
-    if (keyLocation === 2) keyName = 'R';
+    if (keyLocation === 1) keyName = 'l';
+    if (keyLocation === 2) keyName = 'r';
     if (keyLocation === 0) keyName = '';
 
     keyName = keyName + event.key;
-    if (event.key === 'Meta') keyName = 'Meta';
-    keyboardInput(keyName);
+    console.log(keyName);
+    if (event.key === 'Meta') keyName = 'meta';
+    console.log(keyName);
+    keyboardInput(keyName.toLowerCase());
 
 });
 
+
+keyboard.addEventListener('change', (event) => {
+    keyboardSize = event.target.value;
+});
+
+/*need to figure out capital letters
+other board layouts
+*/
